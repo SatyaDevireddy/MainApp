3c57dfb38a06cdc0eecf77dc70539f63e0428bc3
SatyaDevireddy <sddevdev@gmail.com>
2017-06-05 19:48:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var MainController = function ($scope){
+    $scope.Message = "This is a test";
+    var person = {
+        firstName : "Satya",
+        lastName : "Devireddy",
+        image : "none"
+    };
+    $scope.Me = person;
+}
